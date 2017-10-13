52aa8e5c4ac053ff44b156b4ec311bae476e308c
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-13 13:36:07 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -17,5 +17,8 @@ cat /proc/version
 
 cat /proc/cpuinfo
 
+ip_addr=$(ip -4 address | grep global | sed 's/\// /' | awk '{print $2}')
+echo ${ip_addr}
+
 echo apache
 vendor/bin/heroku-php-apache2 -C apache.conf www
