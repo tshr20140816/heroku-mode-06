a6b6b3f87614f11da504ca5abe5886fd0da458cf
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-13 17:51:14 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -21,6 +21,6 @@ export IP_ADDR=$(ip -4 address | grep global | sed 's/\// /' | awk '{print $2}')
 echo ${IP_ADDR}
 
 echo apache
-vendor/bin/heroku-php-apache2 -C apache.conf www
+vendor/bin/heroku-php-apache2 -C apache.conf www &
 
 ./start_worker.sh
