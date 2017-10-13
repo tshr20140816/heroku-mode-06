d4ea8106470972180d5ba4b32e1d5da0ec895dfa
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-13 17:54:07 +0900

    Update start_worker.sh

--- start_worker.sh
+++ start_worker.sh
@@ -10,4 +10,5 @@ echo ${IP_ADDR}
 hostname
 
 echo delegate
-./delegate/delegated -f -r -vvv -P${IP_ADDR}:50080 +=./delegate/delegate.conf
+# ./delegate/delegated -f -r -vvv -P${IP_ADDR}:50080 +=./delegate/delegate.conf
+./delegate/delegated -f -r -vvv -P127.0.0.1:50080 +=./delegate/delegate.conf
