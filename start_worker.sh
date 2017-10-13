200ed19d19648c5c3800836bb8bd90eb11d9b8e0
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-13 13:42:47 +0900

    Update start_worker.sh

--- start_worker.sh
+++ start_worker.sh
@@ -4,5 +4,8 @@ set -x
 
 export TZ=JST-9
 
+export IP_ADDR=$(ip -4 address | grep global | sed 's/\// /' | awk '{print $2}')
+echo ${IP_ADDR}
+
 echo delegate
-./delegate/delegated -f -r -vvv -P50080 +=./delegate/delegate.conf
+./delegate/delegated -f -r -vvv -P${IP_ADDR}:50080 +=./delegate/delegate.conf
