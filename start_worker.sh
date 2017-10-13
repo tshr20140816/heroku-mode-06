75e0b17037335c6f32bed47d578a393f11375229
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-13 10:48:09 +0900

    Update start_worker.sh

--- start_worker.sh
+++ start_worker.sh
@@ -5,4 +5,4 @@ set -x
 export TZ=JST-9
 
 echo delegate
-./delegate/delegated -f -r -v -P127.0.0.1:50080 +=./delegate/delegate.conf
+./delegate/delegated -f -r -vvv -P127.0.0.1:50080 +=./delegate/delegate.conf
