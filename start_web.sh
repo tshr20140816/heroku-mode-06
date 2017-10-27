6844a56972dd3716c74154ba4292572468dd5197
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-27 11:08:22 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -17,4 +17,4 @@ export TZ=JST-9
 #export IP_ADDR=$(ip -4 address | grep global | sed 's/\// /' | awk '{print $2}')
 #echo ${IP_ADDR}
 
-./delegate/delegated -r -vvv -P${PORT} +=./delegate/delegate.conf
+./delegate/delegated -r -vv -P${PORT} +=./delegate/delegate.conf
