55221225f9bd3115c0896e86e618b94f01daf423
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-28 18:16:24 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -4,19 +4,4 @@ set -x
 
 export TZ=JST-9
 
-#printenv
-
-#hostname
-
-#uname -a
-
-#cat /proc/version
-
-#cat /proc/cpuinfo
-
-#export IP_ADDR=$(ip -4 address | grep global | sed 's/\// /' | awk '{print $2}')
-#echo ${IP_ADDR}
-
-whereis php
-
 ./delegate/delegated -r -v -P${PORT} +=./delegate/delegate.conf
