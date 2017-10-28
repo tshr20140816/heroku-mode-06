14703cd60d737555aabc124b82bbed7d1e1b9d4f
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-28 10:23:29 +0900

    Update test.php

--- test.php
+++ test.php
@@ -16,7 +16,7 @@ if (strpos($buf, 'Content-Type: text/html;') !== false)
   $buf = str_replace('<TITLE>', '<HTML><HEAD><META HTTP-EQUIV="REFRESH" CONTENT="600"><TITLE>', $buf);
   $buf = str_replace('</TITLE>', '</TITLE></HEAD>', $buf);
 
-  $buf = str_replace('http://' . $_SERVER['SERVER_NAME'] . ':80/-/builtin/icons/ysato/', './icons/', $buf);
+  $buf = str_replace('http://' . $_SERVER['SERVER_NAME'] . ':80/-/builtin/icons/ysato/', '../icons/', $buf);
 
   $buf = preg_replace('/<FORM ACTION="..\/-search" METHOD=GET>.+?<\/FORM>/s', '<DIV />', $buf);
   
