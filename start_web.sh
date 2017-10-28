1517267e35e83f827507fcd83abc47a79da4ccfe
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-28 20:47:06 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -7,8 +7,6 @@ export TZ=JST-9
 sed -i -e "s/__MAIL_ACCOUNT__/${MAIL_ACCOUNT}/g" ./delegate/delegate.conf
 sed -i -e "s/__PORT__/${PORT}/g" ./delegate/delegate.conf
 
-export KEYWORD01="+pop.${__MAIL_ACCOUNT__}.pop.mail.yahoo.co.jp" 
-
 cat ./delegate/delegate.conf
 
 ./delegate/delegated -r -v -P${PORT} +=./delegate/delegate.conf
