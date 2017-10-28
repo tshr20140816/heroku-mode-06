6531c0200078c17e71c938f346a693b70e0ad440
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-28 09:46:15 +0900

    Update test.php

--- test.php
+++ test.php
@@ -3,10 +3,10 @@
 error_log('***** TEST MESSAGE START *****');
 
 $fp = fopen("php://stdin","r");
-$buf="";
-if ($fp>0) {
-    while(!feof($fp)) $buf .= fread($fp,4092);
-    fclose($fp);
+$buf = "";
+if ($fp > 0) {
+  while(!feof($fp)) $buf .= fread($fp,4092);
+  fclose($fp);
 }
 
 if (strpos($buf, 'Content-Type: text/html;') !== false)
