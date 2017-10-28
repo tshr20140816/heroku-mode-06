2414b9d90ac40d9954aca5ffdf7821a745d7d141
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-28 20:43:30 +0900

    Update filter.php

--- filter.php
+++ filter.php
@@ -47,6 +47,8 @@ if (strpos($header, 'Content-Type: text/html;') !== false || strpos($header, 'Co
   
   $body = str_replace('<A HREF="../"><IMG BORDER=0 ALIGN=MIDDLE ALT="upper" SRC="/icons/up.gif"></A>', '', $body);
   
+  $body = str_replace(getenv('KEYWORD01'), '', $body);
+  
   $body = preg_replace('/^ *\r\n/m', '', $body);
   $body = preg_replace('/^  /m', ' ', $body);
   $body = preg_replace('/^ +</m', '<', $body);
