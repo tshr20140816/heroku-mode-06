edc39fbecbf5d66e75fd1f871afa9e89362536cb
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-28 18:49:45 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -5,11 +5,8 @@ set -x
 export TZ=JST-9
 
 sed -i -e "s/__MAIL_ACCOUNT__/${MAIL_ACCOUNT}/g" ./delegate/delegate.conf
+sed -i -e "s/__APP_NAME__/${APP_NAME}/g" ./delegate/delegate.conf
 
 cat ./delegate/delegate.conf
-
-
-printenv
-hostname
 
 ./delegate/delegated -r -v -P${PORT} +=./delegate/delegate.conf
