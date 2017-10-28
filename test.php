a043de1be5096be676a0dcaa1b773e1e344b40a5
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-28 09:52:44 +0900

    Update test.php

--- test.php
+++ test.php
@@ -13,7 +13,7 @@ if (strpos($buf, 'Content-Type: text/html;') !== false)
 {
   $buf = str_replace('Content-Length:', 'X-Content-Length:', $buf);
   //s/<TITLE>/<HTML><HEAD><META HTTP-EQUIV="REFRESH" CONTENT="600"><TITLE>/g
-  $buf = str_replace('<TITLE>', '<HTML><HEAD><META HTTP-EQUIV="REFRESH" CONTENT="600"><TITLE>', $buf);
+  //$buf = str_replace('<TITLE>', '<HTML><HEAD><META HTTP-EQUIV="REFRESH" CONTENT="600"><TITLE>', $buf);
 
   //$buf = str_replace('http://' . $_SERVER['SERVER_NAME'] . ':80/-/builtin/icons/ysato/', './icons/', $buf);
 
