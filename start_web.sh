b86c055c0b32bfca95ac86d8f4789c3c4ceb3a12
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-29 16:17:24 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -7,8 +7,4 @@ export TZ=JST-9
 apachectl -v
 php --version
 
-sed -i -e "s/__PORT__/${PORT}/g" ./delegate/delegate.conf
-
-cat ./delegate/delegate.conf
-
 ./delegate/delegated -r -v -P${PORT} +=./delegate/delegate.conf
