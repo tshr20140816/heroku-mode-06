379df47e04a05eff61f9dbddfa2136953784a928
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-10-29 16:19:40 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -7,4 +7,8 @@ export TZ=JST-9
 apachectl -v
 php --version
 
-./delegate/delegated -r -v -P${PORT} +=./delegate/delegate.conf
+if [ ${MODE} = 'FRONT' ];
+  vendor/bin/heroku-php-apache2 -C apache.conf www
+else
+  ./delegate/delegated -r -v -P${PORT} +=./delegate/delegate.conf
+fi
