72963c32af5d8a711d7bebfdd511bc5a2aa32de8
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-02 09:38:41 +0900

    Update filter.php

--- filter.php
+++ filter.php
@@ -1,8 +1,16 @@
 <?php
 
+function output_log($message)
+{
+  $stdout = fopen('php://stdout', 'w');
+  fwrite($stdout, $message . "\n");
+  return;        
+}
+
 $pid = getmypid();
 
-error_log($pid . ' ***** FILTER MESSAGE START ***** ' . $_SERVER['REQUEST_URI']);
+//error_log($pid . ' ***** FILTER MESSAGE START ***** ' . $_SERVER['REQUEST_URI']);
+output_log($pid . ' ***** FILTER MESSAGE START ***** ' . $_SERVER['REQUEST_URI']);
 
 error_log($pid . ' ' . $_SERVER['HTTP_USER_AGENT']);
 
