e6f48af84fd9925c8041cb39d6ef2e96f74e604b
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-02 12:45:48 +0900

    Update loggly.php

--- loggly.php
+++ loggly.php
@@ -4,6 +4,7 @@ $pid = getmypid();
 
 error_log($pid . ' ***** LOGGLY STDIN START ***** ');
 //error_log($pid . ' ' . file_get_contents('php://stdin'));
+file_get_contents(getenv('TEST_URL'));
 error_log($pid . ' ***** LOGGLY STDIN FINISH ***** ');
 
 ?>
