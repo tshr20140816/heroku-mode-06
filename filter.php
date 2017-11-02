9cd74931893f136a91d362bfa018544203e47110
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-02 09:24:25 +0900

    Update filter.php

--- filter.php
+++ filter.php
@@ -7,7 +7,7 @@ error_log($pid . ' ***** FILTER MESSAGE START ***** ' . $_SERVER['REQUEST_URI'])
 error_log($pid . ' ' . $_SERVER['HTTP_USER_AGENT']);
 
 
-$url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/http/';
+$url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/' . $_SERVER['SERVER_NAME'] . '/';
 
 error_log($pid . ' ' . $url);
           
