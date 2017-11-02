5458cd9a21ac060010eddceb9852f962f37e15e7
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-02 15:09:23 +0900

    Update loggly.php

--- loggly.php
+++ loggly.php
@@ -2,7 +2,7 @@
 
 $pid = getmypid();
 
-//$buf = file_get_contents('php://stdin');
+$buf = file_get_contents('php://stdin');
 
 //file_get_contents(getenv('TEST_URL') . '?' . urlencode($buf));
 file_get_contents(getenv('TEST_URL') . '?' . $pid);
