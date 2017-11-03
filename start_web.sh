77e381014fbd8f45f5a429a705d907a3885bbfdc
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-03 19:27:07 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -50,5 +50,5 @@ if [ ${MODE} = 'APACHE' ]; then
   
   vendor/bin/heroku-php-apache2 -C apache.conf www
 else
-  ./delegate/delegated -r -v -P${PORT} +=./delegate/delegate.conf
+  ./delegate/delegated -r -v -P${PORT} +=/app/delegate/delegate.conf
 fi
