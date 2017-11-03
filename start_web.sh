53ae27da5097a7ee5d6ce4fa5fad5687f008106e
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-03 13:29:44 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -2,6 +2,16 @@
 
 set -x
 
+if [ ! -v MODE ]; then
+  echo "Error : MODE not defined."
+  exit
+fi
+
+if [ ! -v LOGGLY_TOKEN ]; then
+  echo "Error : LOGGLY_TOKEN not defined."
+  exit
+fi
+
 export TZ=JST-9
 
 apachectl -v
@@ -11,6 +21,34 @@ php --version
 whereis php
 
 if [ ${MODE} = 'APACHE' ]; then
+
+  if [ ! -v LOG_LEVEL ]; then
+    echo "Error : LOG_LEVEL not defined."
+    exit
+  fi
+
+  if [ ! -v BASIC_USER ]; then
+    echo "Error : BASIC_USER not defined."
+    exit
+  fi
+
+  if [ ! -v BASIC_PASSWORD ]; then
+    echo "Error : BASIC_PASSWORD not defined."
+    exit
+  fi
+
+  if [ ! -v REMOTE_PATH_1 ]; then
+    echo "Error : REMOTE_PATH_1 not defined."
+    exit
+  fi
+
+  if [ ! -v REMOTE_PATH_2 ]; then
+    echo "Error : REMOTE_PATH_2 not defined."
+    exit
+  fi
+  
+  htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
+  
   vendor/bin/heroku-php-apache2 -C apache.conf www
 else
   ./delegate/delegated -r -v -P${PORT} +=./delegate/delegate.conf
