c1c56bfbfbc4f8d48856dd1cc9944d39d3cd0371
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-03 11:48:20 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,6 +4,36 @@ set -x
 
 date
 
+if [ ! -v LOG_LEVEL ]; then
+  echo "Error : LOG_LEVEL not defined."
+  exit
+fi
+
+if [ ! -v LOGGLY_TOKEN ]; then
+  echo "Error : LOGGLY_TOKEN not defined."
+  exit
+fi
+
+if [ ! -v BASIC_USER ]; then
+  echo "Error : BASIC_USER not defined."
+  exit
+fi
+
+if [ ! -v BASIC_PASSWORD ]; then
+  echo "Error : BASIC_PASSWORD not defined."
+  exit
+fi
+
+if [ ! -v REMOTE_PATH_1 ]; then
+  echo "Error : REMOTE_PATH_1 not defined."
+  exit
+fi
+
+if [ ! -v REMOTE_PATH_2 ]; then
+  echo "Error : REMOTE_PATH_2 not defined."
+  exit
+fi
+
 export HOME2=${PWD}
 export PATH="${HOME2}/usr/local/bin:${PATH}"
 
