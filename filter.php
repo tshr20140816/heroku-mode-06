84556edccddcff012a781e622359cb0a49dba3b4
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-09 11:58:36 +0900

    Update filter.php

--- filter.php
+++ filter.php
@@ -5,7 +5,7 @@ error_log($pid . ' ***** FILTER MESSAGE START ***** ' . $_SERVER['REQUEST_URI'])
 
 error_log($pid . ' ' . $_SERVER['HTTP_USER_AGENT']);
 
-error_log($pid . ' ' . $url);
+$url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/' . $_SERVER['SERVER_NAME'] . '/';
 
 /*
 $message =
@@ -34,7 +34,12 @@ error_log($pid . ' ' . $res);
 
 if (preg_match('/(Trident|Edge)/', $_SERVER['HTTP_USER_AGENT']))
 {
-  throw new Exception("' #*#*#*#*# IE or Edge #*#*#*#*# '");
+  error_log($pid . ' #*#*#*#*# IE or Edge #*#*#*#*#');
+  header('HTTP', true, 403);
+  
+  
+  
+  error_log($pid . ' ***** FILTER MESSAGE FINISH ***** ' . $_SERVER['REQUEST_URI']);
   return;
 }
 
@@ -116,8 +121,6 @@ if (strpos($header, 'Content-Type: text/html') !== false)
 }
 
 echo $buf;
-
-$url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/' . $_SERVER['SERVER_NAME'] . '/';
 
 $http_status_code = explode(' ', $header, 3)[1];
 
