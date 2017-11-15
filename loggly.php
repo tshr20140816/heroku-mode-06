735a24df8849d5469e1678a13135603f2ee079b5
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-15 15:57:36 +0900

    Update loggly.php

--- loggly.php
+++ loggly.php
@@ -1,11 +1,24 @@
 <?php
+
+$type = $argv[1]; // 'A' or 'E'
+$prefix = $argv[2];
+
 $stdin = fopen('php://stdin', 'r');
 ob_implicit_flush(true);
-while ($line = fgets($stdin))
-{
-  $array = explode(' ', $line, 4);
-  $servername = $array[2];
-  file_put_contents('/app/servername', $servername);
+
+while ($line = fgets($stdin)) {
+  if ($type == 'A') {
+    $array = explode(' ', $line, 3);
+    $servername = $array[1];
+    file_put_contents('/app/servername', $servername);
+  } else {
+    $servername = 'Unknown';
+    if (file_exists('/app/servername')) {
+      $servername = file_get_contents('/app/servername');
+    }
+    $line = "${servername} ${line}";
+  }
+  
   $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/' . $servername . '/';
   
   $context = array(
@@ -14,9 +27,10 @@ while ($line = fgets($stdin))
     "header" => array(
       "Content-Type: text/plain"
       ),
-    "content" => $line
+    "content" => $prefix . ' ' . $line
     )
   );
   $res = file_get_contents($url, false, stream_context_create($context));
 }
+
 ?>
