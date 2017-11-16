3c7a0202c1e791e223b5e5520ad60e3fe4e917b6
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-17 08:56:34 +0900

    Update loggly.php

--- loggly.php
+++ loggly.php
@@ -10,11 +10,26 @@ while ($line = fgets($stdin)) {
   if ($type == 'A') {
     $array = explode(' ', $line, 3);
     $servername = $array[1];
-    file_put_contents('/app/servername', $servername);
+    file_put_contents('/app/SERVER_NAME', $servername);
+    
+    if (file_exists('/app/HOME_IP_ADDRESS')) {
+      $home_ip_address = file_get_contents('/app/HOME_IP_ADDRESS');
+      unlink('/app/HOME_IP_ADDRESS');
+      $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/' . $servername . '/';
+      $context = array(
+        "http" => array(
+          "method" => "POST",
+          "header" => array(
+            "Content-Type: text/plain"
+          ),
+        "content" => $prefix . ' ' . $line
+        ));
+      $res = file_get_contents($url, false, stream_context_create($context));
+    }
   } else {
     $servername = 'Unknown';
-    if (file_exists('/app/servername')) {
-      $servername = file_get_contents('/app/servername');
+    if (file_exists('/app/SERVER_NAME')) {
+      $servername = file_get_contents('/app/SERVER_NAME');
     }
     $line = "${servername} ${line}";
   }
@@ -22,14 +37,13 @@ while ($line = fgets($stdin)) {
   $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/' . $servername . '/';
   
   $context = array(
-  "http" => array(
-    "method" => "POST",
-    "header" => array(
-      "Content-Type: text/plain"
+    "http" => array(
+      "method" => "POST",
+      "header" => array(
+        "Content-Type: text/plain"
       ),
-    "content" => $prefix . ' ' . $line
-    )
-  );
+      "content" => $prefix . ' ' . $line
+    ));
   $res = file_get_contents($url, false, stream_context_create($context));
 }
 
