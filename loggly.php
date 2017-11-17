ccb4fac049b8d6c2a541a0589162a9ed016a7aae
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-17 15:51:41 +0900

    Update loggly.php

--- loggly.php
+++ loggly.php
@@ -23,7 +23,7 @@ while ($line = fgets($stdin)) {
           'header' => array(
             'Content-Type: text/plain'
           ),
-        'content' => "S ${home_ip_address} ${server_name} ${last_update}"
+        'content' => "S ${home_ip_address} * ${server_name} * ${last_update}"
         ));
       $res = file_get_contents($url, false, stream_context_create($context));
     }
