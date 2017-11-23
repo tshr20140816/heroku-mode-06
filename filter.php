4992f1ae59084c998eddd6bc6d90f8d6b68703d3
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-24 02:22:49 +0900

    Update filter.php

--- filter.php
+++ filter.php
@@ -144,6 +144,8 @@ __HEREDOC__;
   $body = preg_replace('/<(s|\/s)mall>/s', '', $body);
   $body = str_replace('</DT>', '</DT><BR><BR>', $body);
   
+  $body = preg_replace('/<FORM ACTION=\/(.+?)\/.+?\/(\d+?)\?ViewerControl METHOD=POST>/s', '<FORM ACTION=/$1/$2?ViewerControl METHOD=POST>', $body);
+  
   // 空白削除
   $body = preg_replace('/^ *\r\n/m', '', $body);
   $body = preg_replace('/^  /m', ' ', $body);
