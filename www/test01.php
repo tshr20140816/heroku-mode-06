abfd61f21bc1ba25a76cc45e5362bddefc14db2d
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-25 12:45:38 +0900

    Update test01.php

--- www/test01.php
+++ www/test01.php
@@ -5,12 +5,13 @@ $encoding = 'shift_jis';
 $global_pattern = '/<tr valign="top">(.+?)</table>/s';
 $item_pattern = '/<td><a href="(.+?)"><img src=".+?".+<h3>(.+?)<\/h3>/s';
 
-$html = file_get_contents($url);
-
 $html = mb_convert_encoding(file_get_contents($url), 'UTF-8', $encoding);
 
-error_log($html);
+//error_log($html);
 
+$rc = preg_match($global_pattern, $html, $matches);
+
+error_log(var_dump($matches));
 /*
 feed_title
 feed_link
