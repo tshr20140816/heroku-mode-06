129250a599f20ba9846ea5817ba7a7d85b11a664
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-25 12:53:54 +0900

    Update test01.php

--- www/test01.php
+++ www/test01.php
@@ -9,10 +9,17 @@ $html = mb_convert_encoding(file_get_contents($url), 'UTF-8', $encoding);
 
 //error_log($html);
 
-$rc = preg_match($global_pattern, $html, $matches);
+$rc = preg_match($global_pattern, $html, $matches1);
+
+//error_log($rc);
+//error_log($matches1[1]);
+
+$rc = preg_match($item_pattern, $matches1[1], $matches2);
 
 error_log($rc);
-error_log($matches[1]);
+error_log($matches2[1]);
+error_log($matches2[2]);
+
 /*
 feed_title
 feed_link
