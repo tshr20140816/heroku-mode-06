f189c2cd0fbe1676a56cce08b58e3244a12685ed
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-06 10:08:02 +0900

    Create install_cli.sh

---
+++ install_cli.sh
@@ -0,0 +1,6 @@
+wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x64.tar.gz -O heroku.tar.gz
+tar xvfz heroku.tar.gz
+cd heroku-cli-v6.15.7-8cc6cd9-linux-x64/bin
+./heroku info
+
+echo 'https://devcenter.heroku.com/articles/dyno-metadata'
