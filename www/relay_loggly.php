a106bb554cb147ef603b0af2b5b2e453e6398f44
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-19 18:06:25 +0900

    Create relay_loggly.php

---
+++ www/relay_loggly.php
@@ -0,0 +1,37 @@
+<?php
+
+if (!isset($_POST['content']) || $_POST['content'] === '') {
+  exit();
+}
+
+if (!isset($_POST['tag']) || $_POST['tag'] === '') {
+  exit();
+}
+
+$content = $_POST['content'];
+$country_name = '';
+
+error_log($content);
+
+if (preg_match('/ \d+\.\d+\.\d+\.\d+ /', $content, $matches) === 1) {
+  $url = 'http://freegeoip.net/json/' . trim($matches[0]);
+  $json = json_decode(file_get_contents($url), true);
+  $country_name = $json['country_name'];
+}
+
+error_log($country_name);
+
+$url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/' . $_POST['tag'] . '/';
+
+$context = [
+  'http' => [
+    'method' => 'POST',
+    'header' => array(
+      'Content-Type: text/plain'
+    ),
+    'content' => $content . ' ' . $country_name
+  ]];
+$res = file_get_contents($url, false, stream_context_create($context));
+
+error_log($res);
+?>
