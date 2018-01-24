f78d44b528b40fabd41ea96745c6aaca7e1e4580
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-24 23:06:03 +0900

    Update loggly_delegate.php

--- loggly_delegate.php
+++ loggly_delegate.php
@@ -7,14 +7,14 @@ while ($line = fgets($stdin)) {
  
   $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . "/tag/${server_name}/";
   
-  $context = array(
-    'http' => array(
+  $context = [
+    'http' => [
       'method' => 'POST',
-      'header' => array(
+      'header' => [
         'Content-Type: text/plain'
-      ),
+      ],
       'content' => "DELEGATE ${line}"
-    ));
+    ]];
   $res = file_get_contents($url, false, stream_context_create($context));
 }
 
