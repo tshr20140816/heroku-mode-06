c82c3bc83312588e2f4fa6e2d9a6e5bb46640258
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-27 14:55:53 +0900

    Update and rename install_cli.sh to 00_special/install_cli.sh

--- 00_special/install_cli.sh
+++ 00_special/install_cli.sh
@@ -4,6 +4,7 @@ set -x
 
 wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x64.tar.gz -O heroku.tar.gz
 tar xvfz heroku.tar.gz
+rm heroku.tar.gz
 cd heroku-cli-*
 cd bin
 ./heroku info
