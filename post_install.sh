8c91101cfcba6628964eb4865f83ab632686a2ea
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-27 20:29:35 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -11,12 +11,17 @@ if [ ! -v DEVELOP_MODE ]; then
   export DEVELOP_MODE='OFF'
 fi
 
-# flying
+wget $(curl https://java.com/en/download/manual.jsp \
+ | grep 'Download Java software for Linux x64"' \
+ | head -n 1 \
+ | grep -oP 'http:.+?BundleId=[0-9a-z_]+') -O java.tar.gz \
+ && tar xvfz java.tar.gz &
+
+wget https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar &
+
 git clone --depth 1 -b 17.4 https://tt-rss.org/git/tt-rss.git ttrss &
 
 git clone --depth 1 https://github.com/tshr20140816/heroku-mode-03.git self_repository &
-
-wget https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar &
 
 # ***** delegate *****
 
