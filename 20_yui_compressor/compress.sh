844949cb0f5e7b45824c6c94556130dbb9dc5884
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-29 04:28:16 +0900

    Update compress.sh

--- 20_yui_compressor/compress.sh
+++ 20_yui_compressor/compress.sh
@@ -2,14 +2,6 @@
 
 set -x
 
-# mv www/ttrss/lib/prototype.js www/ttrss/lib/prototype.js.org
-# time ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type js -o www/ttrss/lib/prototype.js www/ttrss/lib/prototype.js.org
-
-curl https://java.com/en/download/manual.jsp \
- | grep 'Download Java software for Linux x64"' \
- | head -n 1 \
- | grep -oP 'http:.+?BundleId=[0-9a-z_]+'
-
 wget $(curl https://java.com/en/download/manual.jsp \
  | grep 'Download Java software for Linux x64"' \
  | head -n 1 \
@@ -19,7 +11,13 @@ tar xvfz java.tar.gz
 wget https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
 
 for file in $(find /app/www/ttrss/ -name "*.css" -type f -print); do
-  # echo $file;
   mv ${file} ${file}.org
-  ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type css -o ${file} ${file}.org
+  ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type css -o ${file} ${file}.org &
 done
+
+for file in $(find /app/www/ttrss/ -name "*.js" -type f -print); do
+  mv ${file} ${file}.org
+  ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type js -o ${file} ${file}.org &
+done
+
+wait
