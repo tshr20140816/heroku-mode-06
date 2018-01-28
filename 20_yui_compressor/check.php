a069df316a0af1269147687ecd5218066b27d991
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-29 08:58:45 +0900

    Update check.php

--- 20_yui_compressor/check.php
+++ 20_yui_compressor/check.php
@@ -1,5 +1,9 @@
 <?php
 
+echo 'START';
+echo $argv[1];
+echo $argv[2];
+
 $connection_info = parse_url(getenv('DATABASE_URL'));
 $pdo = new PDO(
   "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
@@ -21,8 +25,12 @@ $statement->execute(
   ]);
 
 $result = $statement->fetch();
-  
+
+echo $result['cnt'];
+
 $pdo = null;
+
+echo 'FINISH';
 
 exit($result['cnt']);
 
