876075fda7b8b8f677f19909ad43d9b448a87b34
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-29 04:12:14 +0900

    Update compress.sh

--- 20_yui_compressor/compress.sh
+++ 20_yui_compressor/compress.sh
@@ -5,6 +5,16 @@ set -x
 # mv www/ttrss/lib/prototype.js www/ttrss/lib/prototype.js.org
 # time ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type js -o www/ttrss/lib/prototype.js www/ttrss/lib/prototype.js.org
 
+wget $(curl https://java.com/en/download/manual.jsp \
+ | grep 'Download Java software for Linux x64"' \
+ | head -n 1 \
+ | grep -oP 'http:.+?BundleId=[0-9a-z_]+') -O java.tar.gz
+tar xvfz java.tar.gz
+
+wget https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
+
 for file in $(find /app/www/ttrss/ -name "*.css" -type f -print); do
-  echo $file;
+  # echo $file;
+  mv ${file} ${file}.org
+  ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type css -o ${file} ${file}.org
 done
