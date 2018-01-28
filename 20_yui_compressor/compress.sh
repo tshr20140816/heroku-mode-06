5f3c92a5e2fa2c2d9c16d7ef235e3b2dfb942864
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-29 04:05:45 +0900

    Update compress.sh

--- 20_yui_compressor/compress.sh
+++ 20_yui_compressor/compress.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # mv www/ttrss/lib/prototype.js www/ttrss/lib/prototype.js.org
 # time ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type js -o www/ttrss/lib/prototype.js www/ttrss/lib/prototype.js.org
 
