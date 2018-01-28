cb4c67db9aaf07cf81935679adf029813c50cc88
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-28 17:34:37 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -153,7 +153,7 @@ mv www/ttrss/lib/prototype.js www/ttrss/lib/prototype.js.org
 time ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type js -o www/ttrss/lib/prototype.js www/ttrss/lib/prototype.js.org
 
 mv www/ttrss/js/functions.js www/ttrss/js/functions.js.org
-time ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type css -o www/ttrss/js/functions.js www/ttrss/js/functions.js.org
+time ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type js -o www/ttrss/js/functions.js www/ttrss/js/functions.js.org
 
 rm -rf ttrss
 
