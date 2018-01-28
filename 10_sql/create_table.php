5fa2dcadd139d7092bb7ac23567a1367acf10a8e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-29 02:13:51 +0900

    Create create_table.php

---
+++ 10_sql/create_table.php
@@ -0,0 +1,21 @@
+<?php
+
+$connection_info = parse_url(getenv('DATABASE_URL'));
+$pdo = new PDO(
+  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+  $connection_info['user'],
+  $connection_info['pass']);
+
+$sql = <<< __HEREDOC__
+CREATE TABLE m_application (
+    file_name character varying(255) PRIMARY KEY,
+    file_hash character varying(255) PRIMARY KEY,
+    file bytea NOT NULL,
+    change_time timestamp DEFAULT localtimestamp NOT NULL
+);
+__HEREDOC__;
+$pdo->exec($sql);
+
+$pdo = null;
+
+?>
