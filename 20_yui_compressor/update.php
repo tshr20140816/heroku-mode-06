4cf0ab4a2359a34e166edd22ad948727b0bf4759
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-29 09:37:20 +0900

    Create update.php

---
+++ 20_yui_compressor/update.php
@@ -0,0 +1,13 @@
+<?php
+
+$connection_info = parse_url(getenv('DATABASE_URL'));
+$pdo = new PDO(
+  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+  $connection_info['user'],
+  $connection_info['pass']);
+
+
+
+$pdo = null;
+
+?>
