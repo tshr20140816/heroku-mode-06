fa73065c3064dab188f2c999a484f047e79e70db
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-29 09:40:11 +0900

    Update update.php

--- 20_yui_compressor/update.php
+++ 20_yui_compressor/update.php
@@ -6,7 +6,17 @@ $pdo = new PDO(
   $connection_info['user'],
   $connection_info['pass']);
 
-
+$sql = <<< __HEREDOC__
+INSERT INTO t_file_yui_compressor
+( file_name
+ ,file_hash
+ ,file
+) VALUES (
+  :b_file_name
+ ,:b_file_hash
+ ,:b_file
+)
+__HEREDOC__;
 
 $pdo = null;
 
