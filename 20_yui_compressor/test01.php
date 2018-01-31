75d8841dc215e33018ea147e36bd8776486afac9
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-31 11:42:22 +0900

    Create test01.php

---
+++ 20_yui_compressor/test01.php
@@ -0,0 +1,28 @@
+<?php
+
+$connection_info = parse_url(getenv('DATABASE_URL'));
+$pdo = new PDO(
+  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+  $connection_info['user'],
+  $connection_info['pass']);
+
+$sql = <<< __HEREDOC__
+SELECT file
+  FROM t_file_yui_compressor
+__HEREDOC__;
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute();
+
+$result = $statement->fetch();
+
+error_log($result['file']);
+error_log(pg_unescape_bytea($result['file']));
+file_put_contents('/tmp/test.txt', pg_unescape_bytea($result['file']));
+
+$pdo = null;
+
+exit();
+
+?>
