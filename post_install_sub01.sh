18f6c4b876448e56c436c671bc491cd372e405d3
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-01 11:47:57 +0900

    Update post_install_sub01.sh

--- post_install_sub01.sh
+++ post_install_sub01.sh
@@ -2,4 +2,29 @@
 
 set -x
 
-git clone --depth 1 -b 17.4 https://tt-rss.org/git/tt-rss.git ttrss &
+git clone --depth 1 -b 17.4 https://tt-rss.org/git/tt-rss.git ttrss
+
+mkdir -m 777 -p www/ttrss/css
+cp ttrss/css/* www/ttrss/css/
+
+mkdir -m 777 -p www/ttrss/images
+cp ttrss/images/* www/ttrss/images/
+
+mkdir -m 777 -p www/ttrss/js
+cp ttrss/js/* www/ttrss/js/
+
+mkdir -m 777 -p www/ttrss/lib
+cp -r ttrss/lib/* www/ttrss/lib/
+
+for ext in "${exts[@]}" ; do
+  for file in $(find ./www/ttrss/ -name "*.${ext}" -type f -print); do
+    mv ${file} ${file}.org
+    hash=$(sha512sum ${file}.org | awk '{print $1}')
+    php ./20_yui_compressor/get_file.php ${file} ${hash}
+    if [ $? -eq 0 ]; then
+      mv ${file}.org ${file}
+    fi
+  done
+done
+
+rm -rf ttrss
