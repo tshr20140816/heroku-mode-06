d6dcd3ba0f9e435df78abe75c5877f40ed09d0bd
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-02 11:34:44 +0900

    Update post_install_sub01_01.sh

--- post_install_sub01_01.sh
+++ post_install_sub01_01.sh
@@ -6,6 +6,6 @@ file=$1
 
 mv ${file} ${file}.org
 php ./20_yui_compressor/get_file.php ${file} $(sha512sum ${file}.org | awk '{print $1}')
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
   mv ${file}.org ${file}
 fi
