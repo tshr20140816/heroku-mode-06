b034b1cf50942228ebd97b259d22b100a2a85015
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-02 14:20:44 +0900

    Update create_table.php

--- 10_sql/create_table.php
+++ 10_sql/create_table.php
@@ -6,6 +6,8 @@ $pdo = new PDO(
   $connection_info['user'],
   $connection_info['pass']);
 
+// t_file_yui_compressor
+
 $pdo->query('DROP TABLE t_file_yui_compressor;');
 
 $sql = <<< __HEREDOC__
@@ -24,6 +26,21 @@ __HEREDOC__;
 $pdo->query($sql);
 
 $pdo->query('TRUNCATE TABLE t_file_yui_compressor;');
+
+// t_icon_file
+
+$pdo->query('DROP TABLE t_icon_file;');
+
+$sql = <<< __HEREDOC__
+CREATE TABLE t_icon_file (
+    file_name character varying(255) NOT NULL,
+    file_data text NOT NULL,
+    change_time timestamp DEFAULT localtimestamp NOT NULL
+);
+__HEREDOC__;
+$pdo->query($sql);
+
+$pdo->query('TRUNCATE TABLE t_icon_file;');
 
 $pdo = null;
 
