e0ecedf1b700d9800751072f739c5f18217a8e09
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-02 14:28:28 +0900

    Update geticon.php

--- www/geticon.php
+++ www/geticon.php
@@ -2,6 +2,33 @@
 
 $icon_file_name = $_GET['file_name'];
 
+$connection_info = parse_url(getenv('DATABASE_URL'));
+$pdo = new PDO(
+  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+  $connection_info['user'],
+  $connection_info['pass']);
 
+// t_icon_file
+
+$sql = <<< __HEREDOC__
+SELECT file_data
+  FROM t_icon_file
+ WHERE file_name = :b_file_name
+__HEREDOC__;
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute(
+  [':b_file_name' => $icon_file_name,
+  ]);
+$result = $statement->fetch();
+
+if ($result === FALSE) {
+  // toriniyuku & insert
+} else {
+  // shutsuryoku $result['file_data'])
+}
+
+$pdo = null;
 
 ?>
