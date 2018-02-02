c55593f2a333a0e19c8f17393077a3931e38ba38
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-02 17:33:37 +0900

    Update get_icon.php

--- www/get_icon.php
+++ www/get_icon.php
@@ -1,7 +1,9 @@
 <?php
 
-$icon_file_name = $_GET['file_name'];
+// file_name : /ttrss/feed-icons/nnn.ico
 
+$icon_file_name = pathinfo($_GET['file_name'], PATHINFO_BASENAME);
+  
 $connection_info = parse_url(getenv('DATABASE_URL'));
 $pdo = new PDO(
   "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
