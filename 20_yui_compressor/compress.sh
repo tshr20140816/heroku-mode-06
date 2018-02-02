4b6b3630c43ff9645b0e1250ebbd60e4dd34afe9
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-03 08:17:44 +0900

    Update compress.sh

--- 20_yui_compressor/compress.sh
+++ 20_yui_compressor/compress.sh
@@ -24,18 +24,18 @@ exts[0]='css'
 exts[1]='js'
 
 for ext in "${exts[@]}" ; do
-  for file in $(find /app/www/ttrss/ -name "*.${ext}" -type f -print); do
-    if [ -e ${file}.org ]; then
-      continue
-    fi
-    mv ${file} ${file}.org
-    hash=$(sha512sum ${file}.org | awk '{print $1}')
-    php ./get_file.php ${file} ${hash}
-    if [ $? -ne 0 ]; then
-      ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type ${ext} -o ${file} ${file}.org
-      php update.php ${file} ${hash}
-    else
-      echo -e "pass\n"
-    fi
-  done
+  #for file in $(find /app/www/ttrss/ -name "*.${ext}" -type f -print); do
+  #  if [ -e ${file}.org ]; then
+  #    continue
+  #  fi
+  #  mv ${file} ${file}.org
+  #  hash=$(sha512sum ${file}.org | awk '{print $1}')
+  #  php ./get_file.php ${file} ${hash}
+  #  if [ $? -ne 0 ]; then
+  #    ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type ${ext} -o ${file} ${file}.org
+  #    php update.php ${file} ${hash}
+  #  else
+  #    echo -e "pass\n"
+  #  fi
+  #done
 done
