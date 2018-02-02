9f8dcdcca2b46fcc747e87d7334169ea2614b885
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-02 17:05:59 +0900

    Update get_icon.php

--- www/get_icon.php
+++ www/get_icon.php
@@ -25,7 +25,11 @@ header('Content-Type: image/vnd.microsoft.icon');
 if ($result === FALSE) {
   $result = file_get_contents(getenv('REMOTE_PATH_2') . 'feed-icons/' . $icon_file_name);
   echo $result;
-  // insert
+  $statement = $pdo->prepare('INSERT INTO t_icon_file (file_name, file_data) VALUES (:b_file_name, :b_file_data)');
+  $statement->execute(
+    [':b_file_name' => $icon_file_name,
+     ':b_file_data' => base64_encode($result),
+    ]);
 } else {
   echo base64_decode($result['file_data']);
 }
