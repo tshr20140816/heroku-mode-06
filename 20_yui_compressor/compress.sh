44f3b8d97055fd2aa36b4d4d88d644700f538162
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-02 11:35:19 +0900

    Update compress.sh

--- 20_yui_compressor/compress.sh
+++ 20_yui_compressor/compress.sh
@@ -31,11 +31,11 @@ for ext in "${exts[@]}" ; do
     mv ${file} ${file}.org
     hash=$(sha512sum ${file}.org | awk '{print $1}')
     php ./get_file.php ${file} ${hash}
-    if [ $? -eq 0 ]; then
-      echo -e "pass\n"
-    else
+    if [ $? -ne 0 ]; then
       ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type ${ext} -o ${file} ${file}.org
       php update.php ${file} ${hash}
+    else
+      echo -e "pass\n"
     fi
   done
 done
