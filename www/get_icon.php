b9f193c3c7b3da2b938d0c43ea270c4a17bf0752
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-02 18:53:13 +0900

    Update get_icon.php

--- www/get_icon.php
+++ www/get_icon.php
@@ -28,6 +28,8 @@ $result = $statement->fetch();
 header('Content-Type: image/vnd.microsoft.icon');
 if ($result === FALSE) {
   error_log('File Not Found');
+  error_log(getenv('REMOTE_PATH_2'));
+  error_log(getenv('REMOTE_PATH_2') . 'feed-icons/' . $icon_file_name);
   $result = file_get_contents(getenv('REMOTE_PATH_2') . 'feed-icons/' . $icon_file_name);
   echo $result;
   $statement = $pdo->prepare('INSERT INTO t_icon_file (file_name, file_data) VALUES (:b_file_name, :b_file_data)');
