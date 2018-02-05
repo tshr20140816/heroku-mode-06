751eb3e0c44f3943ff5e2646e27653e22326a0a2
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-05 17:29:35 +0900

    Update relay_loggly.php

--- www/relay_loggly.php
+++ www/relay_loggly.php
@@ -3,19 +3,19 @@
 $pid = getmypid();
 
 if (!isset($_POST['tag']) || $_POST['tag'] === '') {
-  error_log($pid . ' tag is None.');
+  error_log("${pid} tag is None.");
   exit();
 }
 
 if (!isset($_POST['message']) || $_POST['message'] === '') {
-  error_log($pid . ' message is None.');
+  error_log("${pid} message is None.");
   exit();
 }
 
 $message = $_POST['message'];
 $country_name = '';
 
-error_log($pid . ' ' . $message);
+error_log("${pid} ${message}");
 
 if (preg_match('/ \d+\.\d+\.\d+\.\d+ /', $message, $matches) === 1) {
   $ip_address = trim($matches[0]);
@@ -29,7 +29,7 @@ if (preg_match('/ \d+\.\d+\.\d+\.\d+ /', $message, $matches) === 1) {
   }  
 }
 
-error_log($pid . ' ' . $country_name);
+error_log("${pid} ${country_name}");
 
 $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/' . $_POST['tag'] . ',Attack/';
 
@@ -43,5 +43,5 @@ $context = [
   ]];
 $res = file_get_contents($url, false, stream_context_create($context));
 
-error_log($pid . ' ' . $res);
+error_log("${pid} ${res}");
 ?>
