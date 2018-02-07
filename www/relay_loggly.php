a29f9ba1ee7d7c48fc10f048a3b4351061667bb5
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-07 11:41:48 +0900

    Update relay_loggly.php

--- www/relay_loggly.php
+++ www/relay_loggly.php
@@ -19,13 +19,13 @@ error_log("${pid} ${message}");
 
 if (preg_match('/ \d+\.\d+\.\d+\.\d+ /', $message, $matches) === 1) {
   $ip_address = trim($matches[0]);
-  if (file_exists('/tmp/' . $ip_address)) {
-    $country_name = file_get_contents('/tmp/' . $ip_address);
+  if (file_exists("/tmp/${ip_address}")) {
+    $country_name = file_get_contents("/tmp/${ip_address}");
   } else {
     $url = 'http://freegeoip.net/json/' . $ip_address;
     $json = json_decode(file_get_contents($url), true);
     $country_name = $json['country_name'];
-    file_put_contents('/tmp/' . $ip_address, $json['country_name']);
+    file_put_contents("/tmp/${ip_address}", $json['country_name']);
   }  
 }
 
@@ -39,7 +39,30 @@ $context = [
     'header' => [
       'Content-Type: text/plain'
     ],
-    'content' => $message . ' [ ' . $country_name . ' ]'
+    'content' => "${message} [ ${country_name} ]"
+  ]];
+$res = file_get_contents($url, false, stream_context_create($context));
+
+error_log("${pid} ${res}");
+
+if ($country_name != 'Japan') {
+  return;
+}
+
+$res = file_get_contents("http://ipaddress.is/${ip_address}");
+$pos = strpos($res, '<b>IP Whois Information');
+$res = substr($res, $pos);
+$pos = strpos($res, '</tbody>');
+$res = preg_replace('/<.+?>/', "\n", substr($res, 0, $pos));
+$res = preg_replace('/(\n|\r|\r\n)+/us',"\n",$res);
+
+$context = [
+  'http' => [
+    'method' => 'POST',
+    'header' => [
+      'Content-Type: text/plain'
+    ],
+    'content' => $res
   ]];
 $res = file_get_contents($url, false, stream_context_create($context));
 
