f4ef731d12fd2c99c3210e86b322b6c4a150e890
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-17 23:52:11 +0900

    Update post_install_sub01.sh

--- 80_install/post_install_sub01.sh
+++ 80_install/post_install_sub01.sh
@@ -2,21 +2,23 @@
 
 set -x
 
+pushd /tmp
 git clone --depth 1 -b 17.4 https://tt-rss.org/git/tt-rss.git ttrss
+popd
 
-mkdir -m 777 -p www/ttrss/css
-cp ttrss/css/* www/ttrss/css/
+mkdir -m 777 -p ../www/ttrss/css
+cp /tmp/ttrss/css/* ../www/ttrss/css/
 
-mkdir -m 777 -p www/ttrss/images
-cp ttrss/images/* www/ttrss/images/
+mkdir -m 777 -p ../www/ttrss/images
+cp /tmp/ttrss/images/* ../www/ttrss/images/
 
-mkdir -m 777 -p www/ttrss/js
-cp ttrss/js/* www/ttrss/js/
+mkdir -m 777 -p ../www/ttrss/js
+cp /tmp/ttrss/js/* ../www/ttrss/js/
 
-mkdir -m 777 -p www/ttrss/lib
-cp -r ttrss/lib/* www/ttrss/lib/
+mkdir -m 777 -p ../www/ttrss/lib
+cp -r /tmp/ttrss/lib/* www/ttrss/lib/
 
-find ./www/ttrss/ -name "*.css" -type f -print0 | xargs -0i -P 20 -n 1 bash ./post_install_sub01_01.sh {}
-find ./www/ttrss/ -name "*.js" -type f -print0 | xargs -0i -P 20 -n 1 bash ./post_install_sub01_01.sh {}
+# find ../www/ttrss/ -name "*.css" -type f -print0 | xargs -0i -P 20 -n 1 bash ./post_install_sub01_01.sh {}
+# find ../www/ttrss/ -name "*.js" -type f -print0 | xargs -0i -P 20 -n 1 bash ./post_install_sub01_01.sh {}
 
 rm -rf ttrss
