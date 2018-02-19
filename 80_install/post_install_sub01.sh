03cc0afb71f804d6095d9668b911cbcc01407003
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-19 19:44:16 +0900

    Update post_install_sub01.sh

--- 80_install/post_install_sub01.sh
+++ 80_install/post_install_sub01.sh
@@ -4,19 +4,15 @@ set -x
 
 git clone --depth 1 -b 17.4 https://tt-rss.org/git/tt-rss.git /tmp/ttrss
 
-mkdir -m 777 -p www/ttrss/css
-cp /tmp/ttrss/css/* www/ttrss/css/
+mkdir -m 777 -p /tmp/www/ttrss
+mv /tmp/ttrss/css /tmp/www/ttrss/css
+mv /tmp/ttrss/images /tmp/www/ttrss/images
+mv /tmp/ttrss/js /tmp/www/ttrss/js
+mv /tmp/ttrss/lib /tmp/www/ttrss/lib
 
-mkdir -m 777 -p www/ttrss/images
-cp /tmp/ttrss/images/* www/ttrss/images/
-
-mkdir -m 777 -p www/ttrss/js
-cp /tmp/ttrss/js/* www/ttrss/js/
-
-mkdir -m 777 -p www/ttrss/lib
-cp -r /tmp/ttrss/lib/* www/ttrss/lib/
-
+pushd /tmp
 find www/ttrss/ -name "*.css" -type f -print0 | xargs -0i -P 20 -n 1 bash ./80_install/post_install_sub01_01.sh {}
 find www/ttrss/ -name "*.js" -type f -print0 | xargs -0i -P 20 -n 1 bash ./80_install/post_install_sub01_01.sh {}
+popd
 
-rm -rf /tmp/ttrss
+mv /tmp/www/ttrss www/ttrss
