cc95c470424eb917337dfc47c35a893ab416a30d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-19 19:54:32 +0900

    Update post_install_sub01_01.sh

--- 80_install/post_install_sub01_01.sh
+++ 80_install/post_install_sub01_01.sh
@@ -2,10 +2,11 @@
 
 set -x
 
-file=$1
+basepath=$1
+file=$2
 
 mv ${file} ${file}.org
-php ./20_yui_compressor/get_file.php ${file} $(sha512sum ${file}.org | awk '{print $1}')
+php ${basepath}/20_yui_compressor/get_file.php ${file} $(sha512sum ${file}.org | awk '{print $1}')
 if [ $? -ne 0 ]; then
   mv ${file}.org ${file}
 fi
