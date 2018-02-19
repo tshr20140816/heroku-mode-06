3ecc70105b0215b9912a790f6984f85a1c742c2d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-19 19:46:14 +0900

    Update post_install_sub01.sh

--- 80_install/post_install_sub01.sh
+++ 80_install/post_install_sub01.sh
@@ -10,9 +10,11 @@ mv /tmp/ttrss/images /tmp/www/ttrss/images
 mv /tmp/ttrss/js /tmp/www/ttrss/js
 mv /tmp/ttrss/lib /tmp/www/ttrss/lib
 
+CURRENT_DIR=${PWD}
+
 pushd /tmp
-find www/ttrss/ -name "*.css" -type f -print0 | xargs -0i -P 20 -n 1 bash ./80_install/post_install_sub01_01.sh {}
-find www/ttrss/ -name "*.js" -type f -print0 | xargs -0i -P 20 -n 1 bash ./80_install/post_install_sub01_01.sh {}
+find www/ttrss/ -name "*.css" -type f -print0 | xargs -0i -P 20 -n 1 bash ${CURRENT_DIR}/80_install/post_install_sub01_01.sh {}
+find www/ttrss/ -name "*.js" -type f -print0 | xargs -0i -P 20 -n 1 bash ${CURRENT_DIR}/80_install/post_install_sub01_01.sh {}
 popd
 
 mv /tmp/www/ttrss www/ttrss
