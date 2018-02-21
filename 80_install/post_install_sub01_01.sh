5cc7e81198b5c345c99feced99cb0229f414dfb9
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-21 19:14:33 +0900

    Update post_install_sub01_01.sh

--- 80_install/post_install_sub01_01.sh
+++ 80_install/post_install_sub01_01.sh
@@ -6,7 +6,8 @@ echo "START\n"
 
 for file in "$@"; do
   mv ${file} ${file}.org
-  php /tmp/get_file.php ${file}
+  # php /tmp/get_file.php ${file}
+  /tmp/get_file.php ${file}
   if [ $? -ne 0 ]; then
     mv ${file}.org ${file}
   fi
