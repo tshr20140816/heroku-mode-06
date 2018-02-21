b383666922520e4e4a692d873252459e466c8d1a
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-21 18:18:24 +0900

    Update post_install_sub01_01.sh

--- 80_install/post_install_sub01_01.sh
+++ 80_install/post_install_sub01_01.sh
@@ -2,20 +2,20 @@
 
 set -x
 
-file=$1
+#file=$1
 
-mv ${file} ${file}.org
-php /tmp/get_file.php ${file} $(sha512sum ${file}.org | awk '{print $1}')
-if [ $? -ne 0 ]; then
-  mv ${file}.org ${file}
-fi
+#mv ${file} ${file}.org
+#php /tmp/get_file.php ${file} $(sha512sum ${file}.org | awk '{print $1}')
+#if [ $? -ne 0 ]; then
+#  mv ${file}.org ${file}
+#fi
 
 args=("$@")
 
 for file in ${args[@]}; do
-  echo AAAAABBBBBCCCCC${file} 
-#  php /tmp/get_file.php ${file} $(sha512sum ${file}.org | awk '{print $1}')
-#  if [ $? -ne 0 ]; then
-#    mv ${file}.org ${file}
-#  fi
+  mv ${file} ${file}.org
+  php /tmp/get_file.php ${file} $(sha512sum ${file}.org | awk '{print $1}')
+  if [ $? -ne 0 ]; then
+    mv ${file}.org ${file}
+  fi
 done
