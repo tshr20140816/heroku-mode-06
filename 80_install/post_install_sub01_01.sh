ca6cb09f1c896e98de9f135adec5a3b0ae2f6253
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-21 18:34:04 +0900

    Update post_install_sub01_01.sh

--- 80_install/post_install_sub01_01.sh
+++ 80_install/post_install_sub01_01.sh
@@ -12,6 +12,8 @@ echo "START\n"
 #  mv ${file}.org ${file}
 #fi
 
+echo "$@"
+
 for file in "$@"; do
   mv ${file} ${file}.org
   php /tmp/get_file.php ${file} $(sha512sum ${file}.org | awk '{print $1}')
