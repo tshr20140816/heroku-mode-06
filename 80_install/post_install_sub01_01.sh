6333927622af0498e6fedb55501c598ef633c560
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-21 18:14:18 +0900

    Update post_install_sub01_01.sh

--- 80_install/post_install_sub01_01.sh
+++ 80_install/post_install_sub01_01.sh
@@ -10,11 +10,12 @@ if [ $? -ne 0 ]; then
   mv ${file}.org ${file}
 fi
 
-#args=("$@")
+args=("$@")
 
-#for file in ${args[@]}; do
+for file in ${args[@]}; do
+  echo AAAAABBBBBCCCCC${file} 
 #  php /tmp/get_file.php ${file} $(sha512sum ${file}.org | awk '{print $1}')
 #  if [ $? -ne 0 ]; then
 #    mv ${file}.org ${file}
 #  fi
-#done
+done
