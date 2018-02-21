bf2b77799cd16be40f6728710f05f25f0f60d729
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-21 18:44:16 +0900

    Update post_install_sub01.sh

--- 80_install/post_install_sub01.sh
+++ 80_install/post_install_sub01.sh
@@ -15,6 +15,7 @@ cp ./20_yui_compressor/get_file.php /tmp/get_file.php
 
 pushd /tmp
 find www/ttrss/ -name "*.css" -type f -print0 | xargs -0i -P 20 -n 20 bash /tmp/post_install_sub01_01.sh
+find www/ttrss/ -name "*.js" -type f -print0 | xargs -0i -P 20 -n 20 bash /tmp/post_install_sub01_01.sh
 # find www/ttrss/ -name "*.css" -type f -print0 | xargs -0i -P 20 -n 1 bash /tmp/post_install_sub01_01.sh {}
 # find www/ttrss/ -name "*.js" -type f -print0 | xargs -0i -P 20 -n 1 bash /tmp/post_install_sub01_01.sh {}
 popd
