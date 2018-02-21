bd69c98301864541d1d4d536967fc78f2ab59f7e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-21 23:33:24 +0900

    Create get_file2.php

---
+++ 20_yui_compressor/get_file2.php
@@ -0,0 +1,40 @@
+<?php
+
+// $argv[1] : file name (full path)
+// $argv[2] : file hash
+
+$connection_info = parse_url(getenv('DATABASE_URL'));
+$pdo = new PDO(
+  "pgsql:host=${connection_info['host']};dbname=" . substr($connection_info['path'], 1),
+  $connection_info['user'],
+  $connection_info['pass']);
+
+$sql = <<< __HEREDOC__
+SELECT file_data
+  FROM t_file_yui_compressor
+ WHERE file_name = :b_file_name
+   AND file_hash = :b_file_hash
+__HEREDOC__;
+
+$statement = $pdo->prepare($sql);
+
+$statement->execute(
+  [':b_file_name' => pathinfo($argv[1], PATHINFO_BASENAME),
+   ':b_file_hash' => $argv[2],
+  ]);
+
+$result = $statement->fetch();
+
+if ($result === FALSE) {
+  $rc = 1;
+} else {
+  $rc = 0;
+  //error_log($result['file_data']);
+  file_put_contents($argv[1], $result['file_data']);
+}
+
+$pdo = null;
+
+exit($rc);
+
+?>
