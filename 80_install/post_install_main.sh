5576f358699f13ed7f95e350443f94262d08f764
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-02-21 22:57:40 +0900

    Update post_install_main.sh

--- 80_install/post_install_main.sh
+++ 80_install/post_install_main.sh
@@ -19,6 +19,10 @@ bash ./80_install/post_install_sub01.sh &
 
 git clone --depth 1 https://github.com/tshr20140816/heroku-mode-03.git /tmp/self_repository &
 
+# apache
+chmod 777 www
+mkdir -m 777 www/icons
+
 # ***** delegate *****
 
 mkdir -m 777 -p /tmp/usr/bin
@@ -27,10 +31,6 @@ mkdir -m 777 -p /tmp/ccache
 export PATH="/tmp/usr/bin:${PATH}"
 
 mkdir -m 777 -p delegate/icons
-
-# apache
-chmod 777 www
-mkdir -m 777 www/icons
 
 if [ ${DEVELOP_MODE} = 'OFF' ]; then
   mv ccache_cache.zip /tmp/
@@ -43,32 +43,34 @@ export CCACHE_DIR=/tmp/ccache
 export CFLAGS="-O2 -march=native"
 export CXXFLAGS="$CFLAGS"
 
-if [ -e ccache.zip ]; then
-  mv ccache.zip /tmp/usr/bin/
+if [ ! -e 90_binary/delegated ]; then
+  if [ -e ccache.zip ]; then
+    mv ccache.zip /tmp/usr/bin/
+    pushd /tmp/usr/bin
+    unzip ccache.zip
+    popd
+  else
+    pushd /tmp
+    wget https://www.samba.org/ftp/ccache/ccache-3.3.4.tar.gz
+    tar xf ccache-3.3.4.tar.gz
+    pushd ccache-3.3.4
+    ./configure --prefix=/tmp/usr
+    make -j$(grep -c -e processor /proc/cpuinfo)
+    make install
+    popd
+    popd
+  fi
+
   pushd /tmp/usr/bin
-  unzip ccache.zip
+  ln -s ccache gcc
+  ln -s ccache g++
+  ln -s ccache cc
+  ln -s ccache c++
   popd
-else
-  pushd /tmp
-  wget https://www.samba.org/ftp/ccache/ccache-3.3.4.tar.gz
-  tar xf ccache-3.3.4.tar.gz
-  pushd ccache-3.3.4
-  ./configure --prefix=/tmp/usr
-  make -j$(grep -c -e processor /proc/cpuinfo)
-  make install
-  popd
-  popd
+
+  ccache -s
+  ccache -z
 fi
-
-pushd /tmp/usr/bin
-ln -s ccache gcc
-ln -s ccache g++
-ln -s ccache cc
-ln -s ccache c++
-popd
-
-ccache -s
-ccache -z
 
 if [ ! -e delegate9.9.13.tar.gz ]; then
   pushd /tmp
@@ -79,16 +81,21 @@ else
 fi
 pushd /tmp
 tar xf delegate9.9.13.tar.gz
+
 pushd delegate9.9.13
 
-rm ./src/builtin/mssgs/news/artlistfooter.dhtml
-echo "<HR>" > ./src/builtin/mssgs/news/artlistfooter.dhtml
+if [ ! -e 90_binary/delegated ]; then
+  rm ./src/builtin/mssgs/news/artlistfooter.dhtml
+  echo "<HR>" > ./src/builtin/mssgs/news/artlistfooter.dhtml
 
-# time make -j$(grep -c -e processor /proc/cpuinfo) ADMIN="admin@localhost"
-time make ADMIN="admin@localhost"
+  # time make -j$(grep -c -e processor /proc/cpuinfo) ADMIN="admin@localhost"
+  time make ADMIN="admin@localhost"
 
-cp ./src/delegated ${HOME2}/delegate/
-cp ./src/delegated ${HOME2}/www/
+  cp ./src/delegated ${HOME2}/delegate/
+  # cp ./src/delegated ${HOME2}/www/
+else
+  cp 90_binary/delegated ${HOME2}/delegate/
+fi
 cp ./src/builtin/icons/ysato/*.gif ${HOME2}/delegate/icons/
 
 # apache
@@ -99,7 +106,9 @@ cp ${HOME2}/delegate.conf ${HOME2}/delegate/
 popd
 popd
 
-ccache -s
+if [ ! -e 90_binary/delegated ]; then
+  ccache -s
+fi
 
 pushd ${HOME2}
 
