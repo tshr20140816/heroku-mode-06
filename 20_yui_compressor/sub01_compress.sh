aac8676105568ef69024c01c785bf3d723bb8c5b
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-02 12:30:58 +0900

    Update sub01_compress.sh

--- 20_yui_compressor/sub01_compress.sh
+++ 20_yui_compressor/sub01_compress.sh
@@ -13,7 +13,8 @@ mv ${file} ${file}.org
 hash=$(sha512sum ${file}.org | awk '{print $1}')
 php ./get_file2.php ${file} ${hash}
 if [ $? -ne 0 ]; then
-  ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type ${ext} -o ${file} ${file}.org
+  # ./jre*/bin/java -jar ./yuicompressor-2.4.8.jar --type ${ext} -o ${file} ${file}.org
+  /tmp/jre*/bin/java -jar /tmp/yuicompressor-2.4.8.jar --type ${ext} -o ${file} ${file}.org
   php update.php ${file} ${hash}
 else
   echo -e "pass\n"
