a4f4b91e40da12dc493bed6947605ecc0ec5236d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-04 00:41:53 +0900

    Update compress.sh

--- 20_yui_compressor/compress.sh
+++ 20_yui_compressor/compress.sh
@@ -30,8 +30,15 @@ tar xf java.tar.gz
 wget https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
 
 wget https://github.com/tshr20140816/heroku-mode-03/raw/master/bin/brotli
-
 chmod +x brotli
+
+git clone --depth 1 --recursive https://github.com/kornelski/pngquant.git
+pushd pngquant
+./configure --prefix=/tmp/usr
+time make
+make install
+popd
+
 popd
 
 exts[0]='css'
