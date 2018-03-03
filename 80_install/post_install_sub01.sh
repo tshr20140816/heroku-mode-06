da7f61431f122062d3415d09f8655ed378776a20
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-04 01:29:28 +0900

    Update post_install_sub01.sh

--- 80_install/post_install_sub01.sh
+++ 80_install/post_install_sub01.sh
@@ -22,8 +22,9 @@ find www/ttrss/ -name "*.js" -type f -print0 | xargs -0i -P 20 -n 20 php /tmp/ge
 
 find www/ttrss/ -name "*.png" -type f -print0 | xargs -0i -P 2 -n 20 /tmp/usr/bin/pngquant -s1 --ext .png.compress
 
-find www/ttrss/ -name "*.png" -type f -print0 | xargs -0i -P 2 -n 1 bash ./80_install/post_install_sub01_01.sh
 popd
+
+find /tmp/www/ttrss/ -name "*.png" -type f -print0 | xargs -0i -P 2 -n 1 bash ./80_install/post_install_sub01_01.sh
 
 mv /tmp/www/ttrss www/ttrss
 mkdir -m 777 -p www/ttrss/plugins/note
