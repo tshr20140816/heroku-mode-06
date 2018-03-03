5112cf9b3d7f9453b9ef3ef81e5c17b45afe7072
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-04 01:25:41 +0900

    Update post_install_sub01_01.sh

--- 80_install/post_install_sub01_01.sh
+++ 80_install/post_install_sub01_01.sh
@@ -2,3 +2,12 @@
 
 set -x
 
+file=${1}
+
+size_org=$(wc -c < ${file})
+size_comp=$(wc -c < ${file}.compress)
+
+if [ ${size_org} -gt ${size_comp} ]; then
+  mv ${file} ${file}.org
+  mv ${file}.compress ${file}
+fi
