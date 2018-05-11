7f77b8738890724c8b01f62ab1ba1e0fe2cd7144
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-05-11 14:48:25 +0900

    Update relay_rss.php

--- www/relay_rss.php
+++ www/relay_rss.php
@@ -3,5 +3,12 @@
 $if_modified_since = $_SERVER['HTTP_IF_MODIFIED_SINCE'];
 $url = url_decode($_GET['u']);
 
+$ch = curl_init($url);
+
+if ($if_modified_since != NULL) {
+  curl_setopt($ch, CURLOPT_HTTPHEADER, "If-Modified-Since: $if_modified_since");
+}
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+curl_setopt($ch, CURLOPT_ENCODING, "");
 
 ?>
