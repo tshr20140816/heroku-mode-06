4444e9a5eed54f6ed574aef6a8795f0cb99e501d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-05-22 16:58:42 +0900

    Update relay_rss.php

--- www/relay_rss.php
+++ www/relay_rss.php
@@ -9,11 +9,18 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
 curl_setopt($ch, CURLOPT_ENCODING, "");
 
-$rss =  curl_exec($ch);
+$contents = curl_exec($ch);
+
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 curl_close($ch);
 
-header('Content-Type: text/xml);
-echo $rss;
+error_log($http_code);
+error_log($contents);
+
+header('Content-Encoding: gzip');
+header('Content-Type: application/xml');
+  
+echo gzencode($contents, 9);
 
 ?>
