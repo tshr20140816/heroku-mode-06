a38eac9a740d8c591bd21fee09713ffac50b393a
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-13 10:23:13 +0900

    Update loggly.php

--- loggly.php
+++ loggly.php
@@ -7,47 +7,27 @@ $stdin = fopen('php://stdin', 'r');
 ob_implicit_flush(true);
 
 while ($line = fgets($stdin)) {
-  if ($type == 'A') {
-    $array = explode(' ', $line, 3);
-    $server_name = $array[1];
-    file_put_contents('/app/SERVER_NAME', $server_name);
-    
-    if (file_exists('/app/HOME_IP_ADDRESS')) {
-      $home_ip_address = file_get_contents('/app/HOME_IP_ADDRESS');
-      unlink('/app/HOME_IP_ADDRESS');
-      $ip_address = file_get_contents('/app/IP_ADDRESS');
-      unlink('/app/IP_ADDRESS');
-      $last_update = file_get_contents('/app/www/last_update.txt');
-      $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . "/tag/START/";
-      $context = [
-        'http' => [
-          'method' => 'POST',
-          'header' => [
-            'Content-Type: text/plain'
-          ],
-        'content' => "S ${ip_address} * ${server_name} * ${home_ip_address} * ${last_update}"
-        ]];
-      $res = file_get_contents($url, false, stream_context_create($context));
-    }
-  } else {
-    $server_name = 'Unknown';
-    if (file_exists('/app/SERVER_NAME')) {
-      $server_name = file_get_contents('/app/SERVER_NAME');
-    }
+  if ($type == 'E') {
     $line = "${server_name} ${line}";
-  }
-  
-  $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . "/tag/${server_name}/";
-  
-  $context = [
-    'http' => [
-      'method' => 'POST',
-      'header' => [
-        'Content-Type: text/plain'
-      ],
-      'content' => "${prefix} ${line}"
-    ]];
-  $res = file_get_contents($url, false, stream_context_create($context));
+  }  
+  loggly_log("${prefix} ${line}");
 }
 
+exit();
+
+function loggly_log($message_) {
+  $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/' . getenv('HEROKU_APP_NAME') . '/';
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+  curl_setopt($ch, CURLOPT_ENCODING, '');
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: text/plain']);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $message_);
+  curl_exec($ch);
+  curl_close($ch);
+}
 ?>
