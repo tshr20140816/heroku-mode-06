845a25ad0e72c9881c157e17a05489405b95cdfc
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-30 21:42:51 +0900

    Update get_icon.php

--- www/get_icon.php
+++ www/get_icon.php
@@ -28,7 +28,7 @@ $statement->execute(
 $result = $statement->fetch();
 
 if ($result === FALSE) {
-  error_log($log_prefix . 'File Not Found');
+  loggly_log($log_prefix . 'File Not Found');
   error_log($log_prefix . getenv('REMOTE_PATH_2'));
   error_log($log_prefix . getenv('REMOTE_PATH_2') . 'feed-icons/' . $icon_file_name);
   $http_response_header = null;
@@ -71,4 +71,23 @@ if ($result === FALSE) {
 
 $pdo = null;
 
+exit();
+
+function loggly_log($message_) {
+  error_log("${message_}");
+  
+  $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/get_icon,' . getenv('HEROKU_APP_NAME') . '/';
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+  curl_setopt($ch, CURLOPT_ENCODING, '');
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: text/plain']);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $message_);
+  curl_exec($ch);
+  curl_close($ch);
+}
 ?>
