d4df20140d89ad2cd1feddee298bd0b08c624dab
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-30 21:46:46 +0900

    Update loggly.php

--- loggly.php
+++ loggly.php
@@ -6,6 +6,9 @@ $prefix = $argv[2];
 $stdin = fopen('php://stdin', 'r');
 ob_implicit_flush(true);
 
+$ch = null;
+$count = 0;
+
 while ($line = fgets($stdin)) {
   if ($type == 'E') {
     $line = getenv('HEROKU_APP_NAME') . " ${line}";
@@ -17,7 +20,11 @@ exit();
 
 function loggly_log($message_) {
   $url = 'https://logs-01.loggly.com/inputs/' . getenv('LOGGLY_TOKEN') . '/tag/' . getenv('HEROKU_APP_NAME') . '/';
-  $ch = curl_init();
+  
+  global $ch, $count;
+  if ($count % 10 == 0) {
+    $ch = curl_init();
+  }
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
@@ -25,9 +32,12 @@ function loggly_log($message_) {
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
   curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
   curl_setopt($ch, CURLOPT_POST, TRUE);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: text/plain']);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: text/plain', 'Connection: Keep-Alive']);
   curl_setopt($ch, CURLOPT_POSTFIELDS, $message_);
   curl_exec($ch);
-  curl_close($ch);
+  if ($count % 10 == 9) {
+    curl_close($ch);
+  }
+  $count = ($count + 1) % 10;
 }
 ?>
