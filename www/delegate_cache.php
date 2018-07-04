1de55e0f7c289593c570767142f31acce21d4fd3
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-04 10:39:54 +0900

    Update delegate_cache.php

--- www/delegate_cache.php
+++ www/delegate_cache.php
@@ -11,6 +11,10 @@ if (getenv('X_ACCESS_KEY') != $_SERVER['HTTP_X_ACCESS_KEY']) {
 @mkdir('/tmp/ml');
 file_put_contents('/tmp/ml/' . $_SERVER['HTTP_X_FILE_NAME'], $_POST);
 
+error_log("${pid} POSTDATA START");
+error_log($_POST);
+error_log("${pid} POSTDATA FINISH");
+
 $files = scandir('/tmp/ml');
 foreach($files as $file) {
   if ($file == '.' || $file == '..') {
