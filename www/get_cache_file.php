a233b51630643e6730e2a05de3c7c4b71f8d4ac3
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-07 02:54:04 +0900

    Update get_cache_file.php

--- www/get_cache_file.php
+++ www/get_cache_file.php
@@ -1,6 +1,10 @@
 <?php
 $pid = getmypid();
 
+error_log("${pid} ***** SERVER START *****");
+error_log(print_r($_SERVER, true));
+error_log("${pid} ***** SERVER FINISH *****");
+
 error_log("${pid} PHP_AUTH_USER : " . $_SERVER['PHP_AUTH_USER']);
 error_log("${pid} PHP_AUTH_PW : " . $_SERVER['PHP_AUTH_PW']);
 error_log("${pid} RANGE : " . $_GET['range']);
