f978d4b683af962c13d86d90cd938b923b7bc95d
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-06 11:32:19 +0900

    Update get_cache_file.php

--- www/get_cache_file.php
+++ www/get_cache_file.php
@@ -3,16 +3,20 @@ $pid = getmypid();
 
 error_log("${pid} PHP_AUTH_USER : " . $_SERVER['PHP_AUTH_USER']);
 error_log("${pid} PHP_AUTH_PW : " . $_SERVER['PHP_AUTH_PW']);
+error_log("${pid} RANGE : " . $_GET['range']);
 
 switch (true) {
-    case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
+    case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], $_GET['range']):
     case $_SERVER['PHP_AUTH_USER'] !== getenv('BASIC_USER'):
     case $_SERVER['PHP_AUTH_PW'] !== getenv('BASIC_PASSWORD'):
-        header('WWW-Authenticate: Basic realm="dummy"');
+        header('WWW-Authenticate: Basic realm="u"');
         header('Content-Type: text/plain');
         die('NO LOGIN');
 }
 
-header('Content-Type: text/plain');
-echo 'DUMMY';
+$range = $_GET['range'];
+
+header('Content-Type: text/html');
+
+echo file_get_contents('/tmp/ml/' . $range);
 ?>
