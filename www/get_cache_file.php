5065b82f073b2d41cb21cfe6fcf920dd5146200e
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-06 11:18:40 +0900

    Update get_cache_file.php

--- www/get_cache_file.php
+++ www/get_cache_file.php
@@ -1,8 +1,8 @@
 <?php
 $pid = getmypid();
 
-error_log("${pid} " . $_SERVER['PHP_AUTH_USER']);
-error_log("${pid} " . $_SERVER['PHP_AUTH_PW']);
+error_log("${pid} PHP_AUTH_USER : " . $_SERVER['PHP_AUTH_USER']);
+error_log("${pid} PHP_AUTH_PW : " . $_SERVER['PHP_AUTH_PW']);
 
 header('Content-Type: text/plain');
 echo 'DUMMY';
