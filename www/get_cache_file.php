7b2e3f774b1eaa998595f9c15da76ccfd951858b
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-06 11:24:32 +0900

    Update get_cache_file.php

--- www/get_cache_file.php
+++ www/get_cache_file.php
@@ -4,6 +4,15 @@ $pid = getmypid();
 error_log("${pid} PHP_AUTH_USER : " . $_SERVER['PHP_AUTH_USER']);
 error_log("${pid} PHP_AUTH_PW : " . $_SERVER['PHP_AUTH_PW']);
 
+switch (true) {
+    case !isset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']):
+    case $_SERVER['PHP_AUTH_USER'] !== getenv('BASIC_USER'):
+    case $_SERVER['PHP_AUTH_PW'] !== getenv('BASIC_PASSWORD'):
+        header('WWW-Authenticate: Basic realm="dummy"');
+        header('Content-Type: text/plain');
+        die('NO LOGIN');
+}
+
 header('Content-Type: text/plain');
 echo 'DUMMY';
 ?>
