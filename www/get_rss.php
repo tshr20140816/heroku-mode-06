c605bf6d85a09bee61551c8adc7baae82f4e21ae
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-12 11:12:46 +0900

    Update get_rss.php

--- www/get_rss.php
+++ www/get_rss.php
@@ -71,6 +71,7 @@ exit();
 function get_contents($url_) {
   $pid = getmypid();
   $ch = curl_init();
+  /*
   curl_setopt($ch, CURLOPT_URL, $url_); 
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
@@ -78,6 +79,19 @@ function get_contents($url_) {
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
   curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
   curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:56.0) Gecko/20100101 Firefox/60.0');
+  */
+  curl_setopt_array($ch,
+                    [CURLOPT_URL => $url_,
+                     CURLOPT_RETURNTRANSFER => TRUE,
+                     CURLOPT_ENCODING => '',
+                     CURLOPT_CONNECTTIMEOUT => 20,
+                     CURLOPT_FOLLOWLOCATION => TRUE,
+                     CURLOPT_MAXREDIRS => 3,
+                     // CURLOPT_TCP_FASTOPEN => TRUE,
+                     CURLOPT_SSL_FALSESTART => TRUE,
+                     CURLOPT_PATH_AS_IS => TRUE,
+                     CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; rv:56.0) Gecko/20100101 Firefox/61.0',
+                    ]);  
   $contents = curl_exec($ch);
   $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);  
   curl_close($ch);
