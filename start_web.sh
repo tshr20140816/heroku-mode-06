cbe52bd2320ab49124bc1171bed6a763f57dd733
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-23 14:03:03 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -41,7 +41,7 @@ curl -H 'content-type:text/plain' -d "S ${HEROKU_APP_NAME} ${IP_ADDRESS} ${linux
 model_name="$(cat /proc/cpuinfo | grep 'model name' | head -n 1)"
 curl -H 'content-type:text/plain' -d "S ${HEROKU_APP_NAME} ${IP_ADDRESS} ${model_name:13}" ${url}
 
-php_lastest_version=$(http://us1.php.net/downloads.php | grep -o -E 'v7\.2\.[0-9]+')
+php_lastest_version=$(curl http://us1.php.net/downloads.php | grep -o -E 'v7\.2\.[0-9]+')
 curl -H 'content-type:text/plain' -d "S ${HEROKU_APP_NAME} ${IP_ADDRESS} PHP Lastest Version : ${php_lastest_version}" ${url}
 
 php_version="$(php -v | head -n 1)"
