ea86cf492a6107a8403b75f61c9bd0c9ee86cb81
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-07-31 13:06:22 +0900

    Update get_icon.php

--- www/get_icon.php
+++ www/get_icon.php
@@ -81,6 +81,28 @@ $pdo = null;
 
 exit();
 
+function get_contents($url_) {
+  $pid = getmypid();
+  $ch = curl_init();
+  curl_setopt_array($ch,
+                    [CURLOPT_URL => $url_,
+                     CURLOPT_RETURNTRANSFER => TRUE,
+                     CURLOPT_ENCODING => '',
+                     CURLOPT_CONNECTTIMEOUT => 20,
+                     CURLOPT_FOLLOWLOCATION => TRUE,
+                     CURLOPT_MAXREDIRS => 3,
+                     CURLOPT_FILETIME => TRUE,
+                     CURLOPT_PATH_AS_IS => TRUE,
+                     CURLOPT_USERAGENT => 'Love Love Show',
+                    ]);
+  $contents = curl_exec($ch);
+  $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  
+  curl_close($ch);
+  
+  return [$contents, $http_code];
+}
+
 function loggly_log($message_) {
   error_log($message_);
   
