cbdbe0288b80cd38d6e7055242752418730672e1
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-08-20 14:39:59 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -59,11 +59,7 @@ if [ ${MODE} = 'APACHE' ]; then
 
   # apache_lastest_version=$(curl https://github.com/apache/httpd/releases | grep tag-name | head -n 1 | sed -e 's/<[^>]*>//g' | awk '{print $1}')
   # apache_lastest_version=$(curl https://github.com/apache/httpd/releases | grep -o -E '/apache/httpd/releases/tag/2\.4\..+?"' | head -n 1 | grep -o -E '2.+\d')
-  tmp1=$(curl https://github.com/apache/httpd/releases | grep -o -E '/apache/httpd/releases/tag/2\.4\..+?"')
-  echo $tmp1
-  tmp2=$(curl https://github.com/apache/httpd/releases | grep -o -E '/apache/httpd/releases/tag/2\.4\..+?"' | head -n 1)
-  echo $tmp2
-  apache_lastest_version=$(curl https://github.com/apache/httpd/releases | grep -o -E '/apache/httpd/releases/tag/2\.4\..+?"' | head -n 1 | grep -o -E '2.+\d')
+  apache_lastest_version=$(curl https://github.com/apache/httpd/releases | grep -o -E '/apache/httpd/releases/tag/2\.4\..+?"' | head -n 1 | sed -e 's/.\+\///g' | sed -e 's/"//g')
   curl -H 'content-type:text/plain' -d "S ${HEROKU_APP_NAME} ${IP_ADDRESS} Apache Lastest Version : ${apache_lastest_version}" ${url}
 
   apache_version="$(httpd -v)"
